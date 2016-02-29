2ab9d9ef47c7770cb1a9bdfb80e1b0e1416fe59e
xinnix <xinnix2000@gmail.com>
2016-02-29 22:44:14 +0800

    add some views

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-var koa = require('koa');
-var app = koa();
+var express = require('express'),
+    app = express();
 
-app.use(function* (){
-  this.body = 'hello world';
+var port = process.env.PORT || 3000;
+var swig = require('swig');
+
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', './views');
+
+app.use(require('body-parser').urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+
+
+app.get('/', function(req, res) {
+    res.render('index',{hello:'world'} );
+});
+app.get('/signup', function(req, res) {
+    res.render('signup',{} );
+});
+app.get('/memberlist', function(req, res) {
+    res.render('memberlist',{} );
+});
+app.get('/resultinput', function(req, res) {
+    res.render('resultinput',{} );
+});
+app.post('/', function(req, res) {
+    var req_msg = req.body.message;
+    res.send(req_msg);
 });
 
-app.listen('3000');
+app.listen(port);
